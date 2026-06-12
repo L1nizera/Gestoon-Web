9a640788f6eb5301cd45bb74976ad41d212f6f44
paulomussi <127447854+paulomussi@users.noreply.github.com>
2026-06-12 20:33:49 -0300

    kinghosttt222

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,11 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3333",
 });
 
+// const api = axios.create({
+//   baseURL:
+//     import.meta.env.VITE_API_URL || "https://ftp.apigestoon.kinghost.net",
+// });
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("gestoon:token");
 
